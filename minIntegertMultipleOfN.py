fe2f6ec16f8942f1f8dd0dbfdbb5c3eb40514da6
Nishant Thakur <nt.nishantt@gmail.com>
2018-06-23 23:31:43 -0500

    just some random problem
    in Python

---
+++ minIntegertMultipleOfN.py
@@ -0,0 +1,39 @@
+"""
+Give an positive integer n, find out the smallest integer m, such that all digits in m multiply equals to n.
+For example, n = 36, return 49. n = 72, return 89. You can assume there is no overflow.
+"""
+
+def mult_num(num):
+    result = 1
+    for i in num:
+        result *= int(i)
+
+    return result
+
+def min_num_combination(num):
+    l = sorted(list(str(num)))
+    result = ""
+    for i in l:
+        result += i
+    return int(result)
+
+def solution(n):
+    result = int(str(n) + '1')
+
+    for i in range(1,n+1):
+        if n%i == 0:
+            num = str(i)
+            num += str(n//i)
+
+            if n == mult_num(num):
+                res = min_num_combination(num)
+                result = min(result, res)
+
+    return  result
+
+
+
+n = 72
+x = solution(n)
+print(x)
+
---
+++ test.py
@@ -0,0 +1,11 @@
+class Tree:
+    def __init__(self):
+        self.val = 0
+        self.left = None
+        self.right = None
+
+    def set(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+

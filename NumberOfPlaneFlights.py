fbfe950716b0f7dca945ebc5bcdf81cd8a80f687
Nishant Thakur <nt.nishantt@gmail.com>
2018-06-27 01:27:18 -0500

    removed few print statements

--- NumberOfPlaneFlights.py
+++ NumberOfPlaneFlights.py
@@ -1,43 +1,28 @@
 def get_min_number_flights(weights):
     weights = sorted(weights)
-
     i = 0
     j = len(weights)-1
     max_weight = 150
     count = 0
     while i < j:
         while weights[j] > max_weight:
-            print(weights[j],"discarded")
             j -= 1
-            print("count:",count)
 
         if weights[i] > max_weight:
             return count
 
         if weights[i] + weights[j] <= max_weight:
             count += 1
-            print(weights[i], weights[j])
-
             i += 1
             j -= 1
-            print("count:",count)
-
         elif weights[j] <= max_weight:
-            print(weights[j])
             count += 1
             j -= 1
-            print("count:",count)
-
         else:
-            print(weights[i])
             count += 1
             i += 1
-            print("count:",count)
-
         if i == j:
-            print(weights[i])
             count += 1
-            print("count:",count)
 
     return count
 
